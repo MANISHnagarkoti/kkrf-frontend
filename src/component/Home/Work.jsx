03f7b55886d72c6f51a0cfcd0118faed0c83793a
KKRFGROUP <168645376+KKRFGROUP@users.noreply.github.com>
2024-05-15 21:02:59 +0530

    fix popup , contactus and work section

--- src/component/Home/Work.jsx
+++ src/component/Home/Work.jsx
@@ -14,7 +14,7 @@ const Work = () => {
 
       <div className="grid xl:grid-cols-2 gap-24 mt-9">
         <div className="space-y-2">
-          <Link to={active ===  "corporate" && "web-development" }>
+          <Link to={active === "corporate" && "web-development"}>
             <div
               onClick={() => setActive("corporate")}
               className={`py-4  px-3 rounded-2xl transition-all duration-300 relative cursor-pointer  ${
@@ -44,100 +44,103 @@ const Work = () => {
 
               <div
                 className={`text-sm  mt-0  opacity-0  text-gray-700 transition-all duration-300 overflow-hidden h-0 ${
-                  active === "corporate" && `mt-6 opacity-100 h-24`
+                  active === "corporate" && `mt-6 opacity-100 h-36`
                 }`}
               >
-                At KKRF Groups we are specialize in idea-based website designing
-                that help you stand out from the competition. Our team of
-                creative designers and developers collaborate with you to
-                understand your unique vision and business needs and create a
-                customized website that
+                A corporate website serves as the digital face of a company,
+                providing a comprehensive overview of its identity, offerings,
+                and values to various stakeholders. It acts as a central hub for
+                showcasing products or services, sharing company news and
+                updates, and facilitating communication with customers,
+                investors, and job seekers. Corporate websites are meticulously
+                crafted to reflect the brand's personality and establish
+                credibility, often featuring sleek designs, compelling content,
+                and user-friendly interfaces.
               </div>
             </div>
           </Link>
 
-          <Link to={active ===  "ecommerce" && "web-development" }>
-          <div
-            onClick={() => setActive("ecommerce")}
-            className={`py-4  px-3 rounded-2xl transition-all duration-300 relative cursor-pointer  ${
-              active === "ecommerce" && `bg-gray-200/45`
-            }`}
-          >
-            <div className="flex justify-between  font-medium">
-              <div className="text-lg md:text-xl font-semibold">
-                E-Commerce website
-              </div>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className={`w-9 h-9 text-colorTwo`}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            </div>
-
+          <Link to={active === "ecommerce" && "web-development"}>
             <div
-              className={`text-sm  mt-0  opacity-0  text-gray-700 transition-all duration-300 overflow-hidden h-0 ${
-                active === "ecommerce" && `mt-6 opacity-100 h-24`
+              onClick={() => setActive("ecommerce")}
+              className={`py-4  px-3 rounded-2xl transition-all duration-300 relative cursor-pointer  ${
+                active === "ecommerce" && `bg-gray-200/45`
               }`}
             >
-              At KKRF Groups we are specialize in idea-based website designing
-              that help you stand out from the competition. Our team of creative
-              designers and developers collaborate with you to understand your
-              unique vision and business needs and create a customized website
-              that
+              <div className="flex justify-between  font-medium">
+                <div className="text-lg md:text-xl font-semibold">
+                  E-Commerce website
+                </div>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`w-9 h-9 text-colorTwo`}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </div>
+
+              <div
+                className={`text-sm  mt-0  opacity-0  text-gray-700 transition-all duration-300 overflow-hidden h-0 ${
+                  active === "ecommerce" && `mt-6 opacity-100 h-24`
+                }`}
+              >
+                An e-commerce website is an online platform where businesses
+                sell their products or services to customers over the internet.
+                It allows users to browse through a variety of items, make
+                purchases, and complete transactions entirely online.
+              </div>
             </div>
-          </div>
           </Link>
 
-          <Link to={active ===  "ideal" && "web-development" }>
-          <div
-            onClick={() => setActive("ideal")}
-            className={`py-4  px-3 rounded-2xl transition-all duration-300 relative cursor-pointer  ${
-              active === "ideal" && `bg-gray-200/45`
-            }`}
-          >
-            <div className="flex justify-between font-medium">
-              <div className="text-lg md:text-xl font-semibold">
-                Idea Based website
-              </div>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className={`w-9 h-9 text-colorTwo`}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            </div>
-
+          <Link to={active === "ideal" && "web-development"}>
             <div
-              className={`text-sm  mt-0  opacity-0  text-gray-700 transition-all duration-300 overflow-hidden h-0 ${
-                active === "ideal" && `mt-6 opacity-100 h-24`
+              onClick={() => setActive("ideal")}
+              className={`py-4  px-3 rounded-2xl transition-all duration-300 relative cursor-pointer  ${
+                active === "ideal" && `bg-gray-200/45`
               }`}
             >
-              At KKRF Groups we are specialize in idea-based website designing
-              that help you stand out from the competition. Our team of creative
-              designers and developers collaborate with you to understand your
-              unique vision and business needs and create a customized website
-              that
+              <div className="flex justify-between font-medium">
+                <div className="text-lg md:text-xl font-semibold">
+                  Idea Based website
+                </div>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`w-9 h-9 text-colorTwo`}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </div>
+
+              <div
+                className={`text-sm  mt-0  opacity-0  text-gray-700 transition-all duration-300 overflow-hidden h-0 ${
+                  active === "ideal" && `mt-6 opacity-100 h-24`
+                }`}
+              >
+                An idea-based website is a digital platform centered around
+                sharing and exploring concepts, innovations, or creative
+                thoughts. Unlike traditional websites that focus on selling
+                products or providing information, idea-based websites serve as
+                forums for brainstorming, collaboration, and inspiration.
+              </div>
             </div>
-          </div>
           </Link>
         </div>
 
--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -70,75 +70,199 @@ const ContactUs = () => {
   };
 
   return (
-    <div className="container h-screen">
+    <div className="container">
       <ThankyouPop pop={pop} setPop={setPop} />
-      <div className="h-full mt-16">
-        <h1 className="text-6xl font-semibold  text-colorTwo">
-          <span className="text-colorThree font-semibold"> Contact</span> us
-        </h1>
-        <div className="mt-4 text-xl">
-          "Reach out to us. Your message matters."
+
+      <div className=" grid grid-cols-1 lg:grid-cols-2 mt-24 gap-9">
+        <div>
+          <h1 className="text-6xl font-semibold  text-colorTwo">
+            <span className="text-colorThree font-semibold"> Contact</span> us
+          </h1>
+          <div className="mt-4 text-xl">
+            "Reach out to us. Your message matters."
+          </div>
+
+          <div className="h-full rounded-lg overflow-hidden mt-9 md:max-w-[600px]">
+            <form onSubmit={submitData} className="space-y-6">
+              {" "}
+              {/* Form submission */}
+              {/* Name field */}
+              <div>
+                <label>Name</label>
+                <input
+                  type="text"
+                  className="border border-gray-500 p-3"
+                  placeholder="Your name"
+                  name="name"
+                  value={formData.name}
+                  onChange={collectData} // Collect form data
+                />
+              </div>
+              {/* Email field */}
+              <div>
+                <label>Email</label>
+                <input
+                  type="text"
+                  className="border border-gray-500 p-3"
+                  name="email"
+                  placeholder="Your email"
+                  value={formData.email}
+                  onChange={collectData}
+                />
+              </div>
+              {/* Phone field */}
+              <div>
+                <label>Phone</label>
+                <input
+                  type="number"
+                  className="border border-gray-500 p-3"
+                  name="phno"
+                  placeholder="Your phone number"
+                  value={formData.phno}
+                  onChange={collectData}
+                />
+              </div>
+              {/* Query field */}
+              <div>
+                <label>Query</label>
+                <textarea
+                  name="query"
+                  placeholder="Your query"
+                  value={formData.query}
+                  onChange={collectData}
+                  className="border w-full rounded-2xl border-gray-500 p-3"
+                ></textarea>
+              </div>
+              {/* Submit button */}
+              <button
+                type="submit"
+                className="bg-colorThree px-8 py-3 text-white font-semibold text-xs rounded-full mt-9 inline-block"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
 
-        <div className="h-full rounded-lg overflow-hidden mt-9 md:max-w-[600px]">
-          <form onSubmit={submitData} className="space-y-6">
-            {" "}
-            {/* Form submission */}
-            {/* Name field */}
-            <div>
-              <label>Name</label>
-              <input
-                type="text"
-                className="border border-gray-500 p-3"
-                placeholder="Your name"
-                name="name"
-                value={formData.name}
-                onChange={collectData} // Collect form data
-              />
+        <div className="py-9">
+          <div>
+            <div className="text-3xl font-medium">How Can We Help?</div>
+            <div className="mt-4">
+              Please select a topic below related to your inquiry. If you
+              don’t find what you need, fill out our contract form.
             </div>
-            {/* Email field */}
-            <div>
-              <label>Email</label>
-              <input
-                type="text"
-                className="border border-gray-500 p-3"
-                name="email"
-                placeholder="Your email"
-                value={formData.email}
-                onChange={collectData}
-              />
+          </div>
+
+          <div className="mt-9 space-y-6">
+            <div className="flex justify-between border-b border-gray-400 pb-4">
+              <div>
+                <div className="text-base font-semibold">Book a Demo</div>
+                <div className="mt-4 text-gray-500">
+                  Request a demo from one of our conversion specialists.
+                </div>
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </div>
-            {/* Phone field */}
-            <div>
-              <label>Phone</label>
-              <input
-                type="number"
-                className="border border-gray-500 p-3"
-                name="phno"
-                placeholder="Your phone number"
-                value={formData.phno}
-                onChange={collectData}
-              />
+
+            <div className="flex justify-between border-b border-gray-400 pb-4">
+              <div>
+                <div className="text-base font-semibold">Get Inspired</div>
+                <div className="mt-4 text-gray-500">
+                  Discover the many ways in which our customers does the
+                  best.
+                </div>
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </div>
-            {/* Query field */}
-            <div>
-              <label>Query</label>
-              <textarea
-                name="query"
-                placeholder="Your query"
-                value={formData.query}
-                onChange={collectData}
-                className="border w-full rounded-2xl border-gray-500 p-3"
-              ></textarea>
+
+            <div className="flex justify-between border-b border-gray-400 pb-4">
+              <div>
+                <div className="text-base font-semibold">
+                  Ask your queries
+                </div>
+                <div className="mt-4 text-gray-500">
+                  Clear all your doubts with which we can do best for us.{" "}
+                </div>
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </div>
-            {/* Submit button */}
-            <button
-              type="submit"
-              className="bg-colorThree px-8 py-3 text-white font-semibold text-xs rounded-full mt-9 inline-block"
-            >
-              Submit
-            </button>
-          </form>
+          </div>
+
+          <div className="mt-9">
+            <div className="text-2xl font-medium">Contact Us at</div>
+            <div className="flex gap-9 items-center mt-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-5 h-5 text-gray-500"
+              >
+                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+              </svg>
+
+              <div className="underline">info@kkrfgroup.com</div>
+            </div>
+
+            <div className="flex gap-9 items-center mt-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-5 h-5 text-gray-500"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              <div>+91 8595357170</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
